e020ff6ded73c2671a8540d6e84a0ab79ca983a6
Rathan <rathan.kumar492@gmail.com>
2020-06-07 12:53:56 +0530

    setup home and rentals logic

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rentals.models import Rental
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'pages/index.html')
+    rentals = Rental.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'rentals': rentals,
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
--- rentals/views.py
+++ rentals/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from . models import Rental
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 
 
 def index(request):
-    rentals = Rental.objects.all()
+    rentals = Rental.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(rentals, 6)
+    page = request.GET.get('page')
+    paged_rentals = paginator.get_page(page)
     context = {
-        'rentals': rentals
+        'rentals': paged_rentals
     }
     return render(request, 'rentals/rentals.html', context)
 
